8ac9e3b400ce912fb8a4ad56d580db8532312d77
Ace-zzzz <escobia357@gmail.com>
2024-12-14 20:08:49 +0800

    latest

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => 'technology',
+            'name' => fake()->unique()->randomElement(['sports', 'entertainment', 'education', 'technology']),
         ];
     }
 }
